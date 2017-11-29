3165b5321796e5b567e80f9b5c1178d27747dccb
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-11-29 11:23:35 -0600

    initial build working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import App from './App';
+
+//imports Bootstrap and CSS
+import '../node_modules/bootstrap/dist/css/bootstrap-reboot.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css'
+import './index.css';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
